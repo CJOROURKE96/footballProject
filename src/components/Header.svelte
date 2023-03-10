497234723c65fdc6b9ff407955af0c688834936a
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-03-10 14:42:10 +0000

    create svelte project with heading component

---
+++ src/components/Header.svelte
@@ -0,0 +1,11 @@
+<h1>Football For You! ⚽️</h1>
+
+<style>
+    h1 {
+        flex-direction: row;
+        text-align: center;
+        font-weight: bold;
+        color: azure;
+        background-color: teal;
+    }
+</style>
